02f9a1f3e3e191940ccbb66be8770aaa2f2fd032
Saumya <kdevara.saumya@gmail.com>
2017-07-24 22:54:07 -0700

    adding files

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: 'Selena Gomez',
+		photo: 'https://www.filmibeat.com/img/2015/11/24-1448352525-1.jpg',
+		scores: [5, 1, 4, 4, 5, 1]
+	},{
+		name: 'Alex Morgan',
+		photo: 'http://www.famousbirthdays.com/headshots/alex-morgan-3.jpg',
+		scores: [4, 2, 4, 5, 3, 2]
+	},{
+		name: 'Phil Dunphy',
+		photo: 'https://cdn.pastemagazine.com/www/blogs/lists/2012/07/20/phil-dunphy.jpg',
+		scores: [3, 3, 3, 3, 3, 3]
+	},{
+		name: 'Leonardo DiCaprio',
+		photo: 'http://d17zbv0kd7tyek.cloudfront.net/wp-content/uploads/2015/06/leonardo-dicaprio-fb.jpg',
+		scores: [5, 1, 3, 2, 2, 4]
+	}];
+
+module.exports = friends;
