c519d5d098c879959f4fcc94e092fe0fa734ff30
Saumya <kdevara.saumya@gmail.com>
2017-07-31 15:59:43 -0700

    changing port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require('path');
 //=============================================================
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
